29a1500d1279b769a71183408ee766438897b864
Tuyishimire Eric <tuyishimireericc@gmail.com>
2024-01-08 15:24:13 +0200

    Added routes

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const express = require("express")
-    
-    const app = express()
-    
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes"); // new
+
+mongoose
+  .connect("mongodb://localhost:27017/acmedb", { useNewUrlParser: true })
+  .then(() => {
+    const app = express();
+    app.use(express.json());
+    app.use("/api", routes); // new
+
     app.listen(5000, () => {
-        console.log("Server has started!")
-    })
+      console.log("Server has started!");
+    });
+  });
